fd8d2ab8e25742745f7bd9b04a3ff6acd1405697
Ismael <ismaelazd@hotmail.com>
2020-02-11 12:01:07 +0100

    fin des formulaires et faq

--- js/script.js
+++ js/script.js
@@ -11,18 +11,29 @@
     let top = nav.getBoundingClientRect().top + scrollY();
     let width = nav.getBoundingClientRect().width
     let fake = document.createElement('div')
-    fake.style.height = nav.getBoundingClientRect().height+"px";
+    fake.style.height = nav.getBoundingClientRect().height + "px";
     let onScroll = function () {
         let hasScrollClass = nav.classList.contains('sticky')
         if (scrollY() > top && !hasScrollClass) {
             nav.classList.add('sticky')
-            nav.parentNode.insertBefore(fake,nav)
-        }else if(scrollY() < top && hasScrollClass){
+            nav.parentNode.insertBefore(fake, nav)
+        } else if (scrollY() < top && hasScrollClass) {
             nav.classList.remove('sticky')
             nav.parentNode.removeChild(fake)
         }
 
     }
     window.addEventListener('scroll', onScroll)
+
+
+
+    const items = document.querySelectorAll(".accordion a");
+
+    function toggleAccordion() {
+        this.classList.toggle('active');
+        this.nextElementSibling.classList.toggle('active');
+    }
+
+    items.forEach(item => item.addEventListener('click', toggleAccordion));
 
 })()
